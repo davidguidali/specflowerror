99d12916e17d1aecb4d651719d2cea0b5a5aa705
David Guidali <dguidali@greenliff.com>
2020-06-29 19:49:50 +0200

    Added testproject

---
+++ NUnitTestProject1/NUnitTestProject1/SpecFlowFeature1.feature
@@ -0,0 +1,11 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given test1
+
+Scenario: Add trhee numbers
+	Given test2
---
+++ NUnitTestProject1/NUnitTestProject1/SpecFlowFeature1Steps.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+[assembly: Parallelizable(ParallelScope.All)]
+
+namespace NUnitTestProject1
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"test(.*)")]
+        public void GivenTest(int p0)
+        {
+            Thread.Sleep(5000);
+            Assert.IsTrue(true);
+        }
+    }
+}
